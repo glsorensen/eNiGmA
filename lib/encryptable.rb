0702abffd011b5f1520f35d829dcaa7c164e787a
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-01-17 10:17:07 -0700

    Refactor: add pseudocode to cypher method

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -27,34 +27,4 @@ module Encryptable
     end
     encrypted_message
   end
-
-  def decypher(message, un_shift)
-    decrypted_message = ''
-    message.each_char do |char|
-      if valid_characters.include?(char) == false
-        decrypted_message.concat(char)
-      else
-        char_index    = valid_characters.index(char)
-        require 'pry'; binding.pry
-        set_the_wheel = valid_characters.rotate(char_index)
-        decrypted_message.concat(set_the_wheel.rotate((un_shift[0] * -1))[0])
-        un_shift.rotate!
-      end
-    end
-    decrypted_message
-  end
 end
-
-
- #
- #   def cypher(message, shift) # Shifts every valid character in a string and returns the shifted string
- #    counter = -1
- #    message.chars.map do |char|
- #      valid_characters.include?(char) ? shift_char(char, shift[(counter += 1) % 4]) : char
- #    end.join
- #  end
- #
- #  def shift_char(char, shift_num) # shifts a single character by a given amount
- #    index = (valid_characters.index(char) + shift_num) % valid_characters.length
- #    valid_characters[index]
- # end
