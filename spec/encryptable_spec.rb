5822838787a329174c268b0ed7d5a8ca7b53ab14
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-01-17 09:34:42 -0700

    Test: test written for #decypher

--- spec/encryptable_spec.rb
+++ spec/encryptable_spec.rb
@@ -14,11 +14,17 @@ RSpec.describe Encryptable do
     @message = "hello world"
     @key =      "02715"
     @date = "040895"
+    @shift = [3, 27, 73, 20]
+    @encrypt_message = "keder ohulw"
 
     @offset =  "1025"
   end
 
-  it 'shifts a string using given shifts' do
-    expect(cypher(@message, [3, 27, 73, 20])).to eq("keder ohulw")
+  it '#cyper shifts a string using given shifts' do
+    expect(cypher(@message, @shift)).to eq("keder ohulw")
+  end
+
+  it '#decypher shifts a string using given shifts' do
+    expect(decypher(@message, @shift)).to eq @message
   end
 end
