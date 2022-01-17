d6fe07e2fc86defb2d6797aa24b4c32e8b98c580
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-01-17 13:47:40 -0700

    Feat: decrypt runner interaction with Enigma class

--- decrypt.rb
+++ decrypt.rb
@@ -3,3 +3,6 @@ require './lib/enigma'
 encrypted_file = File.open(ARGV[0], "r")
 ciphertext = encrypted_file.read
 encrypted_file.close
+
+decrypting = Enigma.new
+decryption = decrypting.decrypt(ciphertext, ARGV[2], date = ARGV[3] ||= Date.today.strftime("%d%m%y"))
