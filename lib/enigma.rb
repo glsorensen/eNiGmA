dea5a5defad5d7a32b028f45d3d1d63d3888e4c0
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-01-17 08:15:07 -0700

    Feat: add #encrypt hash test, test passed

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,7 @@
 require 'date'
+require './lib/shiftable'
+require './lib/encryptable'
+require './lib/defaultable'
 
 class Enigma
   include Shiftable
@@ -8,12 +11,14 @@ class Enigma
   attr_reader :message,
               :key,
               :date,
+              :encrypted
 
 
   def encrypt(message, key = random_key, date = todays_date)
     @message = message.downcase
     @key     = key
     @date    = date
-    @shift = shift(@key, @date)
+    shift = shift(@key, @date)
+    @encrypted = {encryption: cypher(@message, shift), key: @key, date: @date}
   end
 end
--- spec/encryptable_spec.rb
+++ spec/encryptable_spec.rb
@@ -3,12 +3,15 @@ SimpleCov.start
 require 'date'
 require './lib/encryptable'
 require './lib/defaultable'
+require './lib/shiftable'
+
 
 RSpec.describe Encryptable do
   include Encryptable
   include Defaultable
+  include Shiftable
   before(:each) do
-    @message = "hello world"
+    @message = "he$llo w8*orld"
     @key =      "02715"
     @date = "040895"
 
